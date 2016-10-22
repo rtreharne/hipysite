642bb78a368a9b0f822583b4552a5aff72d16cc5
treharne <R.Treharne@liverpool.ac.uk>
2016-10-22 17:11:35 +0100

    hipy index.html swap

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'sorl.thumbnail'
 )
 
 MIDDLEWARE_CLASSES = (
