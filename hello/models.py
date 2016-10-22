7264ec5047a4ff03edd01ee8935c0d57934cee84
treharne <R.Treharne@liverpool.ac.uk>
2016-10-22 17:19:09 +0100

    Beekeeper model added

--- hello/models.py
+++ hello/models.py
@@ -4,4 +4,14 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
+class Beekeeper(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    twitter = models.URLField(null=True)
+    facebook = models.URLField(null=True)
+    linkedIn = models.URLField(null=True)
+    thumbnail = ImageField(upload_to='beekeeper_thumb', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.last_name
 
