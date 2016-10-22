dd3e90a51422324c2642fe853a833bc69439a234
treharne <R.Treharne@liverpool.ac.uk>
2016-10-22 18:55:02 +0100

    updated views

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from .models import Greeting, Beekeeper
+from .models import Greeting, Beekeeper, Hive, Event
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
+    now = datetime.now()
+    hives = Hive.objects.all()
+    events = []
+    events = Event.objects.filter(finish_time__gte=now).order_by('start_time')
+    event_sub = events[:min(len(events), 3)]
     beekeepers = Beekeeper.objects.all()
-    return render(request, 'index.html', {'beekeepers': beekeepers})
+    
+    return render(request, 'index.html', {'hives': hives, 'events': event_sub, 'beekeepers': beekeepers})
 
 
 def db(request):
