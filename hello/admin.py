e610f934e9c0cb558628daefea72ff65850518c6
treharne <R.Treharne@liverpool.ac.uk>
2016-10-22 17:22:11 +0100

    Beekeeper model added

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from hello.models import Beekeeper
+
+admin.site.register(Beekeeper)
 
 # Register your models here.
--- hello/models.py
+++ hello/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from sorl.thumbnail import ImageField
 
 # Create your models here.
 class Greeting(models.Model):
