4acf24a984ce84485b5ce1f60da3407da88c2f93
treharne <R.Treharne@liverpool.ac.uk>
2016-10-22 18:48:20 +0100

    models updated

--- hello/admin.py
+++ hello/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from hello.models import Beekeeper
+from hello.models import Beekeeper, Event, Hive
 
 admin.site.register(Beekeeper)
+admin.site.register(Event)
+admin.site.register(Hive)
 
 # Register your models here.
--- hello/models.py
+++ hello/models.py
@@ -25,4 +25,11 @@ class Hive(models.Model):
     def __unicode__(self):
         return self.title
 
+class Event(models.Model):
+    hive = models.ForeignKey(Hive)
+    start_time = models.DateTimeField()
+    finish_time = models.DateTimeField(null=False)
+    location = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return self.hive.title
