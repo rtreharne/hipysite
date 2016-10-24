68247ed0fd5db284d4d623002374a9a0621d1a8f
treharne <R.Treharne@liverpool.ac.uk>
2016-10-24 17:33:53 +0100

    settings.py added to collect from /media

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'media'),
 )
 
 MEDIA_URL = '/media/'
