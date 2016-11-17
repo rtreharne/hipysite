87e3c43834fa60b0bb8dbf4964d27b4dcb7f74bc
treharne <R.Treharne@liverpool.ac.uk>
2016-11-17 20:04:39 +0000

    message after sign up, prevent duplicate registration function

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Greeting, Beekeeper, Hive, Event, Sponsor
+from .models import Greeting, Beekeeper, Hive, Event, Sponsor, Registration
 from datetime import datetime
 from hello.forms import RegistrationForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+
+#TODO: create pre-register check function
+def regCheck(event, registration):
+    if Registration.objects.filter(email=registration.email, event=event):
+        return True
+    return False
 
 # Create your views here.
 def index(request):
@@ -22,6 +28,7 @@ def register(request, hive_id=1):
 
     event = Event.objects.get(id=hive_id)
     registrations = event.registrations
+    submitted = None
 
     if request.method == 'POST':
         registration_form = RegistrationForm(data=request.POST)
@@ -29,19 +36,22 @@ def register(request, hive_id=1):
         if registration_form.is_valid():
             reg = registration_form.save(commit=False)
             reg.event = event
-            reg.save()
-            registrations += 1
-            event.registrations = registrations
-
-            event.save()
-
-            return HttpResponseRedirect('/#portfolio')
+            # perform registration check
+            if regCheck(event, reg):
+                submitted = 'Dude, you already registered!'
+            else:
+                reg.save()
+                registrations += 1
+                event.registrations = registrations
+                event.save()
+                submitted = 'Congratulations! You have registered.'
+            return render(request, 'register.html', {'event':event, 'regData': reg, 'registration_form': registration_form, 'submitted':submitted})
         else:
             print registration_form.errors
     else:
         registration_form = RegistrationForm
 
-    return render (request, 'register.html', {'registration_form': registration_form, 'event':event})
+    return render (request, 'register.html', {'registration_form': registration_form, 'event':event, 'submitted': submitted})
 
 def db(request):
 
