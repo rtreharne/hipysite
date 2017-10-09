272a06651a01af42123f697d474ad04ec4d4a9b7
treharne <R.treharne@liverpool.ac.uk>
2017-10-09 17:38:01 +0100

    module page setup and styling complete

--- resources/views.py
+++ resources/views.py
@@ -7,7 +7,15 @@ def modules(request):
 
 def module(request, slug):
     module = Module.objects.filter(slug=slug)[0]
-    elements = Element.objects.filter(module=module).order_by('-id')
+    elements = Element.objects.filter(module=module).order_by('id')
+    for element in elements:
+        if element.github_link:
+            element.github_link = element.github_link.strip('https://').replace('github.com', 'github')
+            element.github_link = 'https://nbviewer.jupyter.org/' + element.github_link
+
+        if element.video_link:
+            element.video_link = element.video_link.split('/')[-1]
+
     return render(request, 'module.html', {'module': module, 'elements': elements})
 
 
